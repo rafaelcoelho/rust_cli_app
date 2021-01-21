b0091c84561659cfdb3ab9e91f1e567177b07825
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-01-20 22:30:35 -0300

    Read from cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess Number");
+
+    println!("Please enter your number: ");
+
+    let mut number = String::new();
+
+    io::stdin()
+    .read_line(&mut number)
+    .expect("Falied to read line");
+
+    println!("Your number is {}", number)
 }
