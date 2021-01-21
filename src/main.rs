9c26ba72209d16371aa981949187d0bb1a19383b
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-01-20 23:14:47 -0300

    Add looping to ask for number

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,34 @@ use std::io;
 fn main() {
     println!("Guess Number");
 
-    println!("Please enter your number: ");
-
     let secrete_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut number = String::new();
+    loop {
+        println!("Please enter your number: ");
 
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Falied to read line");
+        let mut number = String::new();
 
-    let number: u32 = number.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Falied to read line");
 
-    println!("The secret number is: {}", secrete_number);
+        let number: u32 = match number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Only number values are accepted ...");
+                break;
+            }
+        };
 
-    println!("Your number is {}", number);
+        println!("Your number is {}", number);
 
-    match number.cmp(&secrete_number) {
-        Ordering::Less => println!("Too small ..."),
-        Ordering::Greater => println!("Too big ..."),
-        Ordering::Equal => println!("The numbers are equal ..."),
+        match number.cmp(&secrete_number) {
+            Ordering::Less => println!("Too small ..."),
+            Ordering::Greater => println!("Too big ..."),
+            Ordering::Equal => {
+                println!("The numbers are equal ...");
+                break;
+            }
+        }
     }
 }
