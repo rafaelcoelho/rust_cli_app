7f7a76d36ff21bf66fa3d265534d8cf597323239
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-01-22 00:16:17 -0300

    Apply some language concepts
    - Control flow
    - Function definition
    - Duple destruction
    - Statement (never return values) and expression (no semi-colons to return values)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use std::io;
 fn main() {
     println!("Guess Number");
 
+    do_job(100);
+    destruct_object();
+    loop_in();
+
     let secrete_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
@@ -35,4 +39,38 @@ fn main() {
             }
         }
     }
+}
+
+fn destruct_object() {
+    let array = [3; 5];
+
+    println!("a[0] {}", array[0]);
+    println!("a[1] {}", array[1]);
+
+    for number in array.iter() {
+        println!("Numebr is {}", number);
+    }
+
+    let duple: (i32, i64, String) = (10, 20, "ok".to_string());
+    let (value_one, value_two, string_value) = duple;
+
+    println!("n1 is {}", value_one);
+    println!("n2 is {}", value_two);
+    println!("val is {}", string_value);
+}
+
+fn do_job(x: i128) -> i32 {
+    if x > 20 {
+        return 10
+    }
+
+    20
+}
+
+fn loop_in() {
+    for number in (1..10).rev() {
+        println!("{}!", number);
+    }
+
+    println!("Going out");
 }
